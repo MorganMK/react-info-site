c6a8001f979be93fa7ed8650037fff7e746400b6
Kono Steve <stevekono77@gmail.com>
2023-02-02 20:52:44 +0100

    Going to the other one

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import Header from './Header'
-import Footer from './Footer'
+import Index from './components/Index'
+import Navbar from './components/Navbar'
 import './App.css' 
 
 function App() {
   return(
-    <div className="main">
-      <Header/>
-      
-      <h1>Reasons I'm excited to learn React</h1>
-      <ul>
-        <li>React is very popular</li>
-        <li>I'll' be able to fit in with the cool kids at school</li>
-        <li>I will easily transition into mobile development with React Native</li>
-        <li>Making money as a web developer is easier when you master React</li>
-      </ul>
-
-      <Footer/>
+    <div>
+      <Index/>
+      <Navbar/>
     </div>
+    
   )
 }
 
---
+++ src/components/Index.jsx
@@ -0,0 +1,5 @@
+export default function Index(){
+    return(
+        <h1>This is the main section</h1>
+    )
+}
